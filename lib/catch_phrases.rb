ac0f4505491f32a2c695ab71cdcd25c23cee5e66
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2020-12-29 06:40:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,21 +1,25 @@
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-sword = "It's Dangerous To Go Alone! Take This."
-phrase = "It's-a me, Mario!"
-
 def mario
+  phrase = "It's-a me, Mario!"
   puts "#{phrase}"
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts "#{status}"
 end
 
 def link
+  sword = "It's Dangerous To Go Alone! Take This."
   puts "#{sword}"
 end
 
 def all_phrases
+  phrase = "It's-a me, Mario!"
   puts "#{phrase}"
+  
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts "#{status}"
+  
+  sword = "It's Dangerous To Go Alone! Take This."
   puts "#{sword}"
 end
